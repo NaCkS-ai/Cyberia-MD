9d25a0bc19200ecd64cce5b1d2ecf80ce35e6029
Mrcyborg22 <164580391+Mrcyborg22@users.noreply.github.com>
2025-08-01 01:12:14 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~guchVQAI#rHpIsllGzBkJ130w35DQUgf3cyAXFA0rn9W5AlSLc6k",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  PREFIX: "#" // prefix (e.g., ., /, !, *)
 };
